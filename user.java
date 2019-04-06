4effea1114478149017116a4efb28e340bf5bb63
javedkh25 <pjavedkhan91@gmail.com>
2019-04-06 18:24:46 +0530

    commiting

--- user.java
+++ user.java
@@ -22,8 +22,8 @@ public class Prime {
 }
 The best way to learn any programming language is by practicing examples on your own.
 
-This page contains examples on basic concepts of Java programming like: loops, functions, classes, etc.
-
 All programs in this page are tested and verified.
 
 Feel free to use the source code on your system.
+
+29 is a prime number.
